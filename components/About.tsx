560eed38e800b6deaca623ec6d5ad5c527afbdca
AlostXD <junio.ribeiro02@hotmail.com>
2026-05-25 17:50:43 -0300

    Adicionado card do Alisson e Manu

--- components/About.tsx
+++ components/About.tsx
@@ -11,38 +11,9 @@ export default function About() {
       </p>
       <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-6">
       <Cards imageName="vanderlei" Name="Vanderlei Júnio Ribeiro" email="junio.ribeiro02@hotmail.com" phone="+55 34 99942-6661" role="CEO & CTO" internalRole="Desenvolvedor" website="https://portif-lio-zeta.vercel.app/" websiteName="Portfólio"/>
-      <Cards imageName="maria" Name="Maria Eduarda Radtke Pereira" email="dudaradtkelt@gmail.com" phone="+34 655 40 71 85" role="CMO" internalRole="Marketing & Suporte"/>
-        {/* <div className="rounded-xl border border-zinc-800 bg-zinc-900/60 p-5">
-          <h3 className="text-white font-semibold">
-            Alisson Luis Borges Oliveira
-          </h3>
-          <p className="mt-2 text-zinc-300">
-            Suporte, organização de dados e automação aplicada ao dia a dia das
-            empresas.
-          </p>
-          <ul className="mt-3 text-zinc-300 space-y-1">
-            <li>
-              WhatsApp:{" "}
-              <a
-                className="text-red-500 hover:underline"
-                href="https://wa.me/553498390474"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                (+55) 34 99839-0474
-              </a>
-            </li>
-            <li>
-              E-mail:{" "}
-              <a
-                className="text-red-500 hover:underline"
-                href="mailto:alissonluis2009@gmail.com"
-              >
-                alissonluis2009@gmail.com
-              </a>
-            </li>
-          </ul>
-        </div> */}
+      <Cards imageName="maria" Name="Maria Eduarda Radtke Pereira" email="dudaradtkelt@gmail.com" phone="+34 655 40 71 85" role="COO" internalRole="Marketing"/>
+      <Cards imageName="alisson" Name="Alisson Luis Borges Oliveira" email="alissonluis2009@gmail.com" phone="+55 34 99839-0474" role="CIO" internalRole="Suporte"/>
+      <Cards imageName="manu" Name="Emanuely Moreira da Conceição" email="emanuely.moreira7@Outlook.com" phone="+55 27 99887-9921" role="CMO" internalRole="Designer & Social Media"/>
       </div>
     </section>
   );
